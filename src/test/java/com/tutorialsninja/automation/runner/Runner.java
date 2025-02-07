858cc1ff3c1c44e94ee4ea83cae23f83f7fe51ee
divyarani1995 <ranidivya1904@gmail.com>
2025-02-07 22:49:17 +0530

    Completed automating all the scenerios in search feature file

--- src/test/java/com/tutorialsninja/automation/runner/Runner.java
+++ src/test/java/com/tutorialsninja/automation/runner/Runner.java
@@ -11,10 +11,10 @@ import io.cucumber.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"classpath:FeatureFiles/Login.feature" },
+@CucumberOptions(features={"classpath:FeatureFiles/search.feature" },
 glue={"classpath:com.tutorialsninja.automation.stepdef"},
 plugin={"html:target/cucumber_html_report"},
-tags = "@Login and @Four")
+tags = "@Search and @Two")
 public class Runner {
 
 }
---
+++ src/test/java/com/tutorialsninja/automation/stepdef/search.java
@@ -0,0 +1,37 @@
+package com.tutorialsninja.automation.stepdef;
+
+import com.tutorialsninja.automation.framework.Elements;
+import com.tutorialsninja.automation.pages.HeadersSection;
+import com.tutorialsninja.automation.pages.SearchResultPage;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import junit.framework.Assert;
+
+public class search {
+HeadersSection headerSection=new HeadersSection();
+SearchResultPage searchResultPage=new SearchResultPage();
+		@When("I search for a product {string}")
+		public void i_search_for_a_product(String product) {
+			Elements.TypeText(HeadersSection.searchBoxField,product);
+			Elements.click(HeadersSection.searchButton);
+		    
+		}
+		@Then("I should see the product in the search results")
+		public void i_should_see_the_product_in_the_search_results() {
+			Assert.assertTrue(Elements.isDisplayed(SearchResultPage.samsungSyncMasterSearchResult));
+		    
+		}
+
+		
+
+		@Then("I should see a message informing \"([^\\\"]*)\\\"$\"")
+		public void i_should_see_a_page_displaying_the_message(String message) {
+			Assert.assertTrue(Elements.VerifyTextEquals(SearchResultPage.noResultsMessage, message));  
+		}
+
+
+
+
+
+}
